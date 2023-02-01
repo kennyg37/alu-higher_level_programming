9b8c1c7707b69125c7639174368199448922b97e
kennyg37 <k.ganza@alustudent.com>
2023-02-01 03:16:35 -0800

    If statements and loops

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,28 +1,11 @@
 #!/usr/bin/python3                                                                                                                                     
 import random
-
-
-
 number = random.randint(-10000, 10000)
-
 x = str(number)[-1]
-
-
-
 print("Last digit of", number, "is", x, end=" ")
-
-
-
 if x == '0':
-
-        print("and is 0")
-
-    elif x > '5':
-
-            print("and is greater than 5")
-
-        else:
-
-                print("and is less than 6 and not 0")
-
-
+    print("and is 0")
+elif x > '5':
+    print("and is greater than 5")
+else:
+    print("and is less than 6 and not 0")
