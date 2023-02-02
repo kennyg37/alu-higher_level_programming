8c7bc666b034682f647319728d8b5e46492ab006
kennyg37 <k.ganza@alustudent.com>
2023-02-02 02:22:47 -0800

    python if statements and loops

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return number
-if number >= 0:
-    x = number % 10
-else:
-    x = ((number * -1) % 10) * -1
-print("{}".format(number))
+    if number >= 0:
+        x = number % 10
+    else:
+        x = ((number * -1) % 10) * -1
+        print("{}".format(number))
