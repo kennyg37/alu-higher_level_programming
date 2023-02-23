71490a8778c31443ebb8510a6e3dddd7cbeb78cf
kennyg37 <k.ganza@alustudent.com>
2023-02-23 04:05:03 -0800

    python modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import sys
     kk = len(sys.argv)
-    if kk  == 1:
+    if kk == 1:
         print("{} argument.".format(kk - 1))
     else:
         print("{} argument:".format(kk - 1))
     for c in range(1, kk):
-        print("{}: {}".format(c, sys.argv(c)))
+        print("{}: {}".format(c, sys.argv[c]))
