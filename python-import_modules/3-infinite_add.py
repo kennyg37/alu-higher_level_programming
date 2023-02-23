aacde1a44dd78626314a1d1e4c66b374e51bdb2d
kennyg37 <k.ganza@alustudent.com>
2023-02-23 11:35:54 -0800

    python modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    args = sys.argv[1:]
-    add = sum(int(args) for arg in args)
+    length = len(sys.argv)
+    add = 0
+    if length == 1:
+        add = 0
+    else:
+        for c in range(1, lengt):
+            add += int(sys.argv[c])
     print(add)
