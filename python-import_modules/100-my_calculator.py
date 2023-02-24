faa0bd794a73e2920206d097d731de7bafd030e0
kennyg37 <k.ganza@alustudent.com>
2023-02-24 04:08:11 -0800

    python modules

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -20,4 +20,5 @@ if __name__ == "__main__":
         result = mul(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
     print("{} {} {} = {}".format(a, operator, b, result))
