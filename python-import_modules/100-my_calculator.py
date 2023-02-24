114a2d6c34e3275b02c5aac2fcb53c321c9af006
kennyg37 <k.ganza@alustudent.com>
2023-02-24 03:43:11 -0800

    pyhton modules

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __nsme__ =="__main__":
+    import calculator_1
+    import sys
+    length = len(sys.argv)
+    if length < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+    result = none
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+    print("{} {} {} = {}".format(a, operator, b, result))
