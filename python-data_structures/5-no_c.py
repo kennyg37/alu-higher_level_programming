3e2d25115e104e2542ce32d78c4838b0c3be2e6c
kennyg37 <k.ganza@alustudent.com>
2023-03-02 01:02:15 -0800

    data structures

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    # Convert the string to a list of characters
     char_list = list(my_string)
-
-    # Loop over each character in the list and remove 'c' and 'C'
     for char in ['c', 'C']:
         while char in char_list:
             char_list.remove(char)
-
-    # Convert the list of characters back to a string
     new_string = "".join(char_list)
-
-    # Return the modified string
     return new_string
