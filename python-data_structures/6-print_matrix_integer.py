759bf32a7a9c8ac613ff92491b6d3c1010590927
kennyg37 <k.ganza@alustudent.com>
2023-03-02 01:21:04 -0800

    data structures

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for el in row:
-            print("{:d} ".format(el), end="")
+            print("{:d}".format(el), end="")
+            if el != row[-1]:
+                print(end=' ')
         print()
