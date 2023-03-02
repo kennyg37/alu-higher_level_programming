74dba57cf8ce21aabc842647ced39635ed5a9ff6
kennyg37 <k.ganza@alustudent.com>
2023-03-02 00:50:20 -0800

    data structures

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string is None or len(my_string) == 0:
+        return None
+    elif c in my_string:
+        return my_string.remove(c)
+    elif C in my_string:
+        return my_string.remove(C)
+    else:
+        return None
+
