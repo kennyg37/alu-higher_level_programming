eb62258113c742ae8d8bb5d242e24b6ea1410c5b
kennyg37 <k.ganza@alustudent.com>
2023-03-03 02:59:30 -0800

    More Data structures

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0:
-        return None
-    max_num = a_dictionary[0]
-    for i in a_dictionary:
-        if max_num < i:
-            max_num = i
-    return max_num
+    if not a_dictionary:
+        return (None)
+     return (max(a_dictionary, key=a_dictionary.get))
