7145e5fad1880775b4b5f839bb01b2cb98187960
kennyg37 <k.ganza@alustudent.com>
2023-03-03 02:36:04 -0800

    More Data structures

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = dict(map(lambda x: x*2, a_dictionary))
+    new_dict = dict(map(lambda item: (item[0], item[1]*2), a_dictionary.items()))
     return new_dict
