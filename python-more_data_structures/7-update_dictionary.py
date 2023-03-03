c4e0c5a6f437eda4342e7d3832576ccbfd27a5bd
kennyg37 <k.ganza@alustudent.com>
2023-03-03 02:07:09 -0800

    More Data structures

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if not key:
+        return None
+    else:
+        a_dictionary[key] = value
