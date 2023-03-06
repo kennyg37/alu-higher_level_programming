c3cc0b248aa631958e3710a564a788e8a2ddf274
kennyg37 <k.ganza@alustudent.com>
2023-03-06 07:46:12 -0800

    Python Exceptions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,9 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     try:
         i = 0
-       while i < x:
-           print(my_list[])
-           i += 1
+        while i < x:
+            print(my_list[])
+            i += 1
     except IndexError:
         pass 
     finally:
