ba7927f5f3cdc6ba3df73489d2de1979b49daeb8
kennyg37 <k.ganza@alustudent.com>
2023-03-07 10:47:35 -0800

    Python exceptions

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def raise_exception():
-    x = int(input("Name?"))
-    if isinstance(x, int):
-        return x
-    else:
-        raise(TypeError("This is a type error"))
+    x = "27"
+    y = 789
+    if not isinstance(x, int) or not isinstance(y, int):
+        raise TypeError("x and y must be integers")
