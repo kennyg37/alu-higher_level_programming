d8eddac1f8f8f140ef52c76c626bf545de07f9a3
kennyg37 <k.ganza@alustudent.com>
2023-03-07 09:31:53 -0800

    Python Exceptions

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,5 +4,7 @@ def safe_print_division(a, b):
         result = a/b
     except (ZeroDivisionError, TypeError):
         return None
+    else:
+        return result
     finally:
-        print("{} / {} = {}".format(a, b, result))
+        print("Inside result: {}".format(result))
