00f79aecc3eacdf5960a261819365cec09a23da2
kennyg37 <k.ganza@alustudent.com>
2023-03-07 11:35:01 -0800

    Import modules

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
-import functools as f
-print(f.reduce(lambda s, c: s+c, map(lambda c: chr(c), range(65, 91))))
+
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return (sub(a, b)
