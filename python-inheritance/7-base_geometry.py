81c4a9309cf0495f8d5fd63e657396a0368ed5f8
kennyg37 <k.ganza@alustudent.com>
2023-03-22 19:15:06 +0200

    Python Inheritance

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" define an empty class."""
+
+
+class BaseGeometry:
+    """ an empty class"""
+
+    def area(self):
+        """Not done."""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        elif value < 0:
+            raise ValueError("<name> must be greater than 0")
