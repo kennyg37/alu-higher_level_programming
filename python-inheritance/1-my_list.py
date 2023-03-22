f4c1e472299ccddbab9a611359ba461e1d325305
kennyg37 <k.ganza@alustudent.com>
2023-03-22 11:33:06 +0200

    Python Inheritance

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Method documented """
+
+
+class MyList(List):
+    """ Class documented """
+    def __init__(self, myList):
+        self.__myList = myList
+
+    def print_sorted(self):
+        self.__myList = dir(myList)
+        return self.__myList
