6f777e501161be20d35d846dc30fbeaee9066e15
kennyg37 <k.ganza@alustudent.com>
2023-03-22 18:30:09 +0200

    Python Inheritance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module documented"""
+
+
+def is_some_class(obj, a_class):
+    """ Method documented """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
