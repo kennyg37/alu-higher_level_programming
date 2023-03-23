32a9286114e6092fcd956e689e3c03c04fd45521
kennyg37 <k.ganza@alustudent.com>
2023-03-23 16:59:57 +0200

    Python Input and output

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""Defines a file-appending function."""
+"""
+Defines a file-appending function.
+"""
 
 
 def write_file(filename="", text=""):
-     """Appends a string to the end of a UTF8 text file.
-    Args:
-        filename (str): The name of the file to append to.
-        text (str): The string to append to the file.
-    Returns:
-        The number of characters appended.
-    """
-    with open(filename, "a", encoding="utf-8") as c:
+     """Appends a string to the end of a UTF8 text file"""
+    with open(filename, "w", encoding="utf-8") as c:
         return c.write(text)
