6ef93b7e40cc275d2daf902a6c4776df310f4428
kennyg37 <k.ganza@alustudent.com>
2023-03-23 21:21:10 +0200

    Python Inheritance

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,41 +1,34 @@
-#!/usr/bin/python3
-''' prasing the lofg '''
-
-
 import sys
 
+# Define counters and variables
+status_code_count = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+total_file_size = 0
+line_count = 0
 
-def print_status():
-    '''
-        Printing the status of the request
-    '''
-    counter = 0
-    size = 0
-    file_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
+try:
+    # Read input line by line
+    for line in sys.stdin:
+        line_count += 1
 
-    for l in sys.stdin:
-        line = l.split()
-        try:
-            size += int(line[-1])
-            code = line[-2]
-            status_codes[code] += 1
-        except:
-            continue
-        if counter == 9:
-            print("File size: {}".format(size))
-            for key, val in sorted(status_codes.items()):
-                if (val != 0):
-                    print("{}: {}".format(key, val))
-            counter = 0
-        counter += 1
-    if counter < 9:
-        print("File size: {}".format(size))
-        for key, val in sorted(status_codes.items()):
-            if (val != 0):
-                print("{}: {}".format(key, val))
+        # Split the input line into fields
+        fields = line.split(" ")
+        status_code = fields[8]
+        file_size = fields[9]
 
+        # Update counters
+        total_file_size += int(file_size)
+        status_code_count[status_code] += 1
 
-if __name__ == "__main__":
-    print_status()
+        # Print metrics every 10 lines
+        if line_count % 10 == 0:
+            print("File size: {}".format(total_file_size))
+            for code, count in sorted(status_code_count.items()):
+                if count > 0:
+                    print("{}: {}".format(code, count))
+
+except KeyboardInterrupt:
+    # Print final metrics on keyboard interruption
+    print("File size: {}".format(total_file_size))
+    for code, count in sorted(status_code_count.items()):
+        if count > 0:
+            print("{}: {}".format(code, count)))
