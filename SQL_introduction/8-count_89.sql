ebb5e1390f96d54a7522200ec2f9764a88f0b849
kennyg37 <k.ganza@alustudent.com>
2023-03-27 21:34:41 +0200

    SQL introduction

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- inserts a new row in the table first_table (database hbtn_0c_0)
-INSERT INTO first_table VALUES ( 89, 'Holberton School');
+-- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server
+SELECT COUNT(id) FROM first_table WHERE id=89;
