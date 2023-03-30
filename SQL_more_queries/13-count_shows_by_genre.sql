453355b77ced3564253d73d40580f96a3b8d668b
kennyg37 <k.ganza@alustudent.com>
2023-03-30 16:42:08 +0200

    SQL introduction

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,4 @@
+-- querie that uses inner join to filter out shows that do not fill the condition
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
