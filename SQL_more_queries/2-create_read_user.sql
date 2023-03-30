e276079ab1b8fa39535b1bee763d40793eb90751
kennyg37 <k.ganza@alustudent.com>
2023-03-30 11:00:07 +0200

    SQL More queries

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- creating database and user
-CREATE DATABASE 'hbtn_0d_2' AND User 'user_0d_2'@'localhost' IF NOT EXISTS IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE hbtn_0d_2;
+CREATE User  IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT PRIVILEGES ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
 
