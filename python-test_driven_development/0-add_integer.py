61fa837b7dec67dac2bccee616fa49ea18c180dd
kennyg37 <k.ganza@alustudent.com>
2023-04-24 11:29:04 +0200

    initial commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+""" Module documented by Ken Ganza
+This code creates a function that adds two integers and returns an error when one is of the parameters is not an integer
+"""
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
+    """ This FUnction perfoms the task at hand"""
+    if not isinstance(a, (int, float)): 
+        raise TypeError("a must be an integer")
+    elif not isinstance(a, (int, float)):
+        raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
     return a + b
