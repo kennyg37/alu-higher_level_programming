7553d19787b8377f5c18435600205ab4bba15609
kennyg37 <k.ganza@alustudent.com>
2023-04-25 11:17:00 +0200

    initial commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@
 class Base:
     """ Class Base is being used to define all objects and methods"""
     _nb_objects = 0
-    
+
     def __init__(self, id=None):
         if id is not None:
             self.id = id
