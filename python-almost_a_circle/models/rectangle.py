9f97f16cd2680db344413023db91df19362ecf1b
kennyg37 <k.ganza@alustudent.com>
2023-04-25 13:17:35 +0200

    initial commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,10 +11,9 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
-    
+
     @property
     def width(self):
-        """Set/get the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -27,16 +26,37 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """Set/get the height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
         if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
+             raise TypeError("height must be an integer")
+         if value <= 0:
+             raise ValueError("height must be > 0")
+         self.__height = value
 
     @property
+    def x(self):
+        return self.__x
 
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Set/get the y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
