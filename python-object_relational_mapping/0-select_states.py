38b3364f2a3cb261123f44b3295dcf658ddb6aac
kennyg37 <k.ganza@alustudent.com>
2023-04-26 08:45:25 +0200

    initial commit

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    kd = MYSQLdb.connect(
+    kd = MySQLdb.connect(
             host="localhost", port=3306, user=argv[1],
             password=argv[2], database=argv[3])
     cur = kd.cursor()
