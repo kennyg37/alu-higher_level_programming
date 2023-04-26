d86005f753021ec9346714fd4bfbedf171854608
kennyg37 <k.ganza@alustudent.com>
2023-04-26 08:55:47 +0200

    initial commit

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" fetches all the names starting by m in the database"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1], 
+            password=argv[2], database=argv[3])
+    curser = conn.cursor()
+    curser.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    kd = cursor.fetchall()
+    for x in kd:
+        print(x)
