aa180489d9517ce6e90d377a5985d499f97efa3b
kennyg37 <k.ganza@alustudent.com>
2023-06-13 18:27:45 +0200

    initial commit

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-function factorial(num) {
-	if (isNaN(num)) {
-		return 1;
-	} else if (num === 0) {
-		return 1;
-	} else {
-		return num * factorial(num - 1);
-	}
-};
+function factorial (num) {
+  if (isNaN(num)) {
+    return 1;
+  } else if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
 const input = parseInt(process.argv[2], 10);
 console.log(factorial(input));
-
