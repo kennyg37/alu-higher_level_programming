f14805de0f1b5278492ef10b777d50d4345ffcad
kennyg37 <k.ganza@alustudent.com>
2023-06-13 18:45:12 +0200

    initial commit

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const big = process.argv.slice(2).map(Number);
+const biggest = big.sort((a, b) => b - a);
+if (big.length <= 1) {
+  console.log(0);
+} else {
+  console.log(biggest[1]);
+}
