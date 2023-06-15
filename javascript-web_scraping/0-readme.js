31d9154b4978c7da9c469326179c1ffecffcb649
kennyg37 <k.ganza@alustudent.com>
2023-06-15 20:10:11 +0200

    initial commit

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const file = process.argv[2];
+const fs = require('fs');
+fs.readfile(file, 'utf8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
