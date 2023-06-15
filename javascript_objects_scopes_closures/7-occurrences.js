38801a860501a603aa9a5633f403fe3e27ca88a3
kennyg37 <k.ganza@alustudent.com>
2023-06-15 15:58:23 +0200

    initial commit

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.nbOcurrences = function (list, searchElement) {
+exports.nbOccurrences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
