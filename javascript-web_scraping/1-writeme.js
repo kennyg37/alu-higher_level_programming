84d5c53df09ec76004c1a1600d7f516d1a38c2cb
kennyg37 <k.ganza@alustudent.com>
2023-06-15 22:15:56 +0200

    initial commit

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const input = process.argv[2];
 const newString = process.argv[3];
 const fs = require('fs');
-fs.writeFile(input, 'utf8', (error, newString) => {
+fs.writeFile(input, newString, 'utf8', (error)  => {
   if (error) {
     console.log(error);
   } else {
