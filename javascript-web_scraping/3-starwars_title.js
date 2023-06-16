8407eb290e740100bfcad83948f469efb3c0f3ac
kennyg37 <k.ganza@alustudent.com>
2023-06-16 11:10:31 +0200

    initial commit

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const ApiEndpoint = 'https://swapi-api.alx-tools.com/api/films/:id';
+request.get(ApiEndpoint, (error, response, body) => {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    const ApiMovieId = JSON.parse(body);
+    console.log(ApiMovieId.title);
+  }
+});
