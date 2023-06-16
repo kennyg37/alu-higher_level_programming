7859f175546aab52e0a854b8f03f099bec8281f7
kennyg37 <k.ganza@alustudent.com>
2023-06-16 11:59:15 +0200

    initial commit

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    const todos = JSON.parse(body);
+    const dash = {};
+    for (let i = 0; i < todos.length; i++) {
+      const statuss = (todos[i].completed);
+      const key = todos[i].userId.toString();
+      if (statuss) {
+        if (dash[key]) {
+          dash[key]++;
+        } else {
+          dash[key] = 1;
+        }
+      }
+    }
+    console.log(dash);
+  }
+});
